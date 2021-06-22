287d1ed65f04b13d38797b684315735a15e5821a
dispnt <yuyuanjiajian@gmail.com>
2021-06-22 22:00:22 +0800

    add sortbyprice label

--- code.user.js
+++ code.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name         Skeb Price Helper
+// @name         Skeb Price Helper Test
 // @namespace    https://fofu.dispnt.com/
-// @version      0.3
+// @version      0.3.0
 // @description  Get Price info in your Skeb's following creators Page
 // @author       Dispnt
 // @match        https://skeb.jp/*/following_creators*
 // @icon         https://www.google.com/s2/favicons?domain=skeb.jp
 // @grant        GM_xmlhttpRequest
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
 
 
@@ -14,8 +15,8 @@
 (function () {
     'use strict';
     function getArtistPrice() {
-        var artist_cols = document.getElementsByClassName('column');    // get artist collections
-        Array.prototype.forEach.call(artist_cols, function (artist_col) {   // forEach
+        var artist_cols = document.getElementsByClassName('column'); // get artist collections
+        Array.prototype.forEach.call(artist_cols, function (artist_col) { // forEach
             if (artist_col.innerText.includes('Seeking')) { // Skip artist isn't seeking
                 var artist_url = "https://skeb.jp" + artist_col.childNodes[0].getAttribute("href"); //concatenate url
                 GM_xmlhttpRequest({ // get current artist's page
@@ -23,10 +24,10 @@
                     url: artist_url,
                     onload: function (response) {
                         if (response.status == 200) {
-                            let artist_page = new DOMParser().parseFromString(response.responseText, "text/html");  // parse text to html
-                            let price = artist_page.getElementsByTagName("td")[3].innerText;    // find price label
-                            artist_col.setAttribute("price",price.replace(/[^0-9]/ig,""));  // set price integer to attribute
-                            artist_col.getElementsByClassName("subtitle is-7")[0].innerText = price;    //set subtitle to price
+                            let artist_page = new DOMParser().parseFromString(response.responseText, "text/html"); // parse text to html
+                            let price = artist_page.getElementsByTagName("td")[3].innerText; // find price label
+                            artist_col.setAttribute("price",price.replace(/[^0-9]/ig,"")); // set price integer to attribute
+                            artist_col.getElementsByClassName("subtitle is-7")[0].innerText = price; //set subtitle to price
                         }
                     }
                 });
@@ -34,8 +35,25 @@
             }
         });
     }
+
+    function sortByPrice(){
+        alert("in Process")
+    }
+
+    function createPriceSortLabel() {
+        var div = document.createElement("div");
+        div.innerHTML = "<a class='is-active' style='color: #473C8B'>*Price*</a>";
+        div.class = "level-item";
+        var level = document.getElementsByClassName("level-right")[0];
+        level.appendChild(div);
+        div.addEventListener ("click", function() {
+            sortByPrice();
+        });
+    }
+
     setTimeout(function () {
         console.log('🦙 Skeb Price Helper is now running... 🦙');
+        createPriceSortLabel()
         getArtistPrice();
     }, 1000);
 
